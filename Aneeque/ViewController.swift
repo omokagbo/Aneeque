ecae25a0e6b8598afa014ef5f79f935c9d303b69
Emmanuel Marabe Omokagbo <emmanuel.omokagbo@gmail.com>
2021-10-24 01:06:59 +0100

    Initial Commit

---
+++ Aneeque/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aneeque
+//
+//  Created by Decagon on 24/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
